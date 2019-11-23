0a64b6749ffc4376a315f4e8b612a5a6fba9b9a7
darwin1224 <darwinblacks@gmail.com>
2019-11-23 21:41:39 +0700

    refactor(model): add validation in customer model

--- src/modules/customer/models/CustomerModel.ts
+++ src/modules/customer/models/CustomerModel.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { IsNotEmpty, IsString, Length } from 'class-validator';
 
 @Entity('tbl_customer')
 export class CustomerModel {
@@ -16,5 +17,8 @@ export class CustomerModel {
    * @type {string}
    */
   @Column({ type: 'varchar', length: 50 })
+  @IsNotEmpty()
+  @IsString()
+  @Length(4, 50)
   public readonly customer_name!: string;
 }
